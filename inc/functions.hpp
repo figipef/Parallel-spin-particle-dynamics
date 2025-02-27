c82617fa7a170df6fc73c2395a6523a749467799
ist1102821 <ist1102821@tecnico.ulisboa.pt>
2025-02-27 11:26:25 +0000

    added the diagnostics function for future development, and added the possibility to fully determine the diagnostic that we might want to perform

--- inc/functions.hpp
+++ inc/functions.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "particle.hpp"
+#include <fstream>
 
 int test(int);
 
@@ -8,3 +9,6 @@ double* cross(double[3], double[3]);
 double gamma(double[3]);
 void boris(Particle*, double, double[3], double[3], int);
 void createParticles(Particle*, int);
+void PerformDiagnostics(std::ifstream&, Particle*,  \
+	int , std::string , std::string , std::string , double , double , double, \
+	double , double , double , double , double , double );
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include <sstream>
 #include <unordered_map>
 
-
 // CONSTANTS (almost)
 double MASS = 1; 
 double CHARGE = 1;
@@ -40,6 +39,25 @@ int main() {
     int particle_number = std::stoi(values["NUMBER_OF_PARTICLES"]);
     double time_step = std::stod(values["TIME_STEP"]);
     int total_time = std::stoi(values["TOTAL_TIME"]);
+
+    // Save the Diagnostics parameters to strings
+    std::string par1 = values["PAR1"];
+    std::string par2 = values["PAR2"];
+    std::string par3 = values["PAR3"];
+
+    // Save the wanted Bin data
+    double bin1_size = std::stod(values["BIN_SIZE_1"]);
+    double bin2_size = std::stod(values["BIN_SIZE_2"]);
+    double bin3_size = std::stod(values["BIN_SIZE_3"]);
+
+    double bin1_max = std::stod(values["B1_MAX"]);
+    double bin2_max = std::stod(values["B2_MAX"]);
+    double bin3_max = std::stod(values["B3_MAX"]);
+
+    double bin1_min = std::stod(values["B1_MIN"]);
+    double bin2_min = std::stod(values["B2_MIN"]);
+    double bin3_min = std::stod(values["B3_MIN"]);
+
 
     // ============================
     //   START OF MAIN CODE BLOCK
