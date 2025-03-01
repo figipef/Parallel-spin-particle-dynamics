40d230d7fc5821e7f1199993818c5ebfc3be74d3
Afonso Santos <afonso.carmo.santos@gmail.com>
2025-03-01 13:00:52 +0000

    vector calculus funcs added

--- inc/functions.hpp
+++ inc/functions.hpp
@@ -4,10 +4,12 @@
 #include <vector>
 #include <fstream> // Removable
 #include <iostream>
+#include <cmath> 
 
 int test(int);
 
 double* cross(double[3], double[3]);
+double inner(double[3], double[3]);
 double gamma(double[3]);
 void boris(Particle*, double, double[3], double[3], int);
 void createParticles(Particle*, int);
