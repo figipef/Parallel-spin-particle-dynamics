de2dcd98f82b490a4a86f6610638f4f902a61160
Afonso Santos <afonso.carmo.santos@gmail.com>
2025-03-09 00:09:13 +0000

    Added plotting functions to Plotter.py. Also updater the input reader

--- src/Plotter.py
+++ src/Plotter.py
@@ -1,5 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_v_time(q, dt, Title = "", ylabel = "Values", Grid = True, Lims = 0): #takes and array q of some quantity and the time step between values and plots the data
+    n = len(q)
+    t = np.linspace(0., dt*n, n)
+
+    plt.figure(figsize=(8,5))
+    plt.plot(t, q)
+
+    plt.xlabel('Time (units are overrated)', fontsize=16) #I will eventually uncover our dimessions
+    plt.ylabel(ylabel, fontsize=16)
+    plt.title(Title, fontsize=18)
+
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+
+    if isinstance(Lims, (list, tuple)):
+        plt.xlim((Lims[0][0], Lims[0][1]))
+        plt.ylim((Lims[1][0], Lims[1][1]))
+
+    plt.grid(Grid)
+    plt.show()
+
+def plot_lots_v_time(Q, dt, Title = "", ylabel = "Values", Grid = True, Lims = 0, labels = ["Quantity 1", "Quantity 2"]): #takes and array of arrays q of some quantities and the time step between values and plots the data
+    N = len(Q)
+    if N != len(labels):
+        labels = ["Quantity " + str(i + 1) for i in range(N)]
+    
+    plt.figure(figsize=(8,5))
+    
+    for i in range(N):
+        q = Q[i]
+        n = len(q)
+        t = np.linspace(0., dt*n, n)
+
+        plt.plot(t, q, label = labels[i])
+
+    plt.xlabel('Time (units are overrated)', fontsize=16) #I will eventually uncover our dimessions
+    plt.ylabel(ylabel, fontsize=16)
+    plt.title(Title, fontsize=18)
+    plt.legend(fontsize=15)
+
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+
+    if isinstance(Lims, (list, tuple)):
+        plt.xlim((Lims[0][0], Lims[0][1]))
+        plt.ylim((Lims[1][0], Lims[1][1]))
+
+    plt.grid(Grid)
+    plt.show()
+
 #num primeiro aproach precisamos de plots tipo quantidade vs tempo assumindo que recebe a lista de outputs e o dicionario de input do reader
+    #done
+
 #Depois tambem haveremos de precisar de histogramas para espaço de fases
--- src/main.py
+++ src/main.py
@@ -7,6 +7,9 @@ print(v)
 print('-----------------------------')
 print(v[:,1])
 print('-----------------------------')
-v = R.read_input("src/input_example.txt")
-print(v)
+d = R.read_input("src/input_example.txt")
+print(d)
 
+#P.plot_v_time(v[:,0], d['TIME_STEP'], Title="TESTING", ylabel="I honestly forgor", Grid=False, Lims=((0.,15.),(-2,3)))
+
+P.plot_lots_v_time([v[:,0], v[:,1], v[:,2]], d['TIME_STEP'], Title="TESTING", ylabel="I honestly forgor", Grid=False, Lims=((0.,15.),(-2,3)), labels=[d["PAR1"], "spin y", d["PAR3"]])
